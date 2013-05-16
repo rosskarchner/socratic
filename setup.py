4517e1a10b97d654e03abfc2078d6b4537d97b78
Ross M Karchner <ross.karchner@me.com>
2013-05-16 15:00:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
-__author__="Aiden Scandella"
+__author__="Ross Karchner, after Aiden Scandella"
 __date__ ="$Jan 11, 2010 5:06:00 PM$"
 
 from setuptools import setup,find_packages
 
 setup (
-  name = 'Socrata-Python',
+  name = 'socratic',
   version = '0.2',
   packages = find_packages(),
 
   install_requires=['feedparser','requests','poster'],
 
-  author = 'Aiden Scandella',
-  author_email = 'aiden.scandella@socrata.com',
-
-  url = 'http://www.socrata.com',
+  author = 'Ross Karchner, after Aiden Scandella',
+  author_email = 'ross@karchner.com',
   license = '',
   long_description= 'A native Python implementation of Socrata\'s REST API, using JSON'
 )
