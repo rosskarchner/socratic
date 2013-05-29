4cd6e09e93acf86823e56e4341c0a20371494801
Ross Karchner <ross.karchner@cfpb.gov>
2013-05-29 13:02:43 -0400

    removed all debug references

--- src/socratic/socrata.py
+++ src/socratic/socrata.py
@@ -19,40 +19,39 @@ def authenticated_session():
     return session
 
 class API(object):
-    def __init__(self, host=None, session=None, debug=False):
+    def __init__(self, host=None, session=None):
         self.session = session or request.Session()
         self.host = host or os.environ.get('SOCRATA_HOST')
 
-    def call(self, resource='', method="GET", data=None, headers=None, json=True, files=None, debug=False):
+    def call(self, resource='', method="GET", data=None, headers=None, json=True, files=None):
         url = urljoin (self.host, resource)
         client = getattr(self.session,method.lower())
         resp = client(url, data=data, headers=headers, files=files)
-        if debug:
-            print resp.content
         return resp
 
     def call_publish(self,view_id, operation=None):
         publish_url = "/api/views/%s/publication.json" % view_id
         if operation:
             publish_url += "?method=%s" % operation
-        resp = self.call(publish_url, method="POST")
         while True:
+            resp = self.call(publish_url, method="POST")
             if resp.status_code == 200:
                 metadata = json.loads(resp.content)
                 return metadata['id']
             elif resp.status_code != 202:
                 resp.raise_for_status()        
+
          
         
     def create_working_copy(self, original_view):
         return self.call_publish(original_view, operation="copy")  
             
     def publish_working_copy(self, working_copy):
-        return self.call_publish(original_view)  
+        return self.call_publish(working_copy)  
 
     def wait_for_ticket(self, ticket):
         time.sleep(120)
-        resp = self.call('/api/imports2.json?ticket=%s' % ticket, debug=True)
+        resp = self.call('/api/imports2.json?ticket=%s' % ticket)
         if resp.status_code == 202:
             message = json.loads(resp.content)
             print "import status: %s Sleeping for a minute" % message['details']['status']
@@ -61,11 +60,11 @@ class API(object):
             dataset_id = json.loads(resp.content)['id']
             return dataset_id
 
-    def upload(self, path, replace=None, append=None, blueprint_path=None, debug=False):
+    def upload(self, path, replace=None, append=None, blueprint_path=None):
         f=open(path)
         filename=os.path.split(path)[1]
 
-        response= self.call('/api/imports2?method=scan', method='POST', files={filename:f}, debug=debug)
+        response= self.call('/api/imports2?method=scan', method='POST', files={filename:f})
         parsed_response = json.loads(response.content)
         file_id= parsed_response['fileId']
 
@@ -87,7 +86,7 @@ class API(object):
         if blueprint_path:
             bp_data= file(blueprint_path).read()
             import_data['blueprint'] = bp_data
-        import_response = self.call('/api/imports2.json%s' % extra_args, method='POST', data=import_data, debug=debug)
+        import_response = self.call('/api/imports2.json%s' % extra_args, method='POST', data=import_data)
         return import_response
 
 
